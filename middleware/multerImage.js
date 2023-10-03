83201e30c01120bb77cfa5ef673d6d0bdd3dfbeb
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-10-03 22:22:49 +0300

    Format omitted.

--- middleware/multerImage.js
+++ middleware/multerImage.js
@@ -13,8 +13,7 @@ const imgConfig = multer.diskStorage({
 
 //* image filter
 const fileFilter = (req, file, callback) => {
-  if (file.mimetype === "image/webp") {
-    //* Accepts only .webp files
+  if (file.mimetype === "image") {
     callback(null, true);
   } else {
     callback(new Error("Only .webp images are allowed"), false);
--- routes/propertiesRoutes.js
+++ routes/propertiesRoutes.js
@@ -2,11 +2,16 @@ const express = require("express");
 const router = express.Router();
 const propertiesControllers = require("../controllers/propertiesController");
 const upload = require("../middleware/multerImage");
+const converter = require("../middleware/imageConverter");
 
 router
   .route("/")
   .get(propertiesControllers.getAllProperties)
-  .post(upload.array("files", 5), propertiesControllers.createNewProperty)
+  .post(
+    upload.array("files", 5),
+    converter,
+    propertiesControllers.createNewProperty
+  )
   .patch(upload.array("files", 5), propertiesControllers.updateProperty)
   .delete(propertiesControllers.deleteProperty);
 
