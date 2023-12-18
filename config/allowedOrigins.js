ac3edfe8567a476650fe20a082b0957bb346d59e
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-12-18 13:28:21 +0300

    Allowed origins added.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,6 +3,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://deja-vu-reve.vercel.app",
   "https://dejavu-cms.vercel.app",
+  "https://panel.dejavureve.com",
+  "http://panel.dejavureve.com",
 ];
 
 module.exports = allowedOrigins;
