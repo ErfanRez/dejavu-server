8f7ff8197404409ddd3f597985544b003214750f
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2024-02-06 13:08:00 +0300

    New private port added.

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,6 @@
 const allowedOrigins = [
   "http://localhost:3000",
+  "http://localhost:4862",
   "http://localhost:5173",
   "https://deja-vu-reve.vercel.app",
   "https://dejavu-cms.vercel.app",
